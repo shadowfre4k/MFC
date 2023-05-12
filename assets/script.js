5dc51f0ccd3c99c232ea1e5fcef8def8783e8ece
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-05-11 20:44:01 -0700

    basic html and css set-up

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,12 @@ var init = function () {
 };
 
 init();
+
+var omdbApiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=bd8b9b41";
+var youtubeSearchApiUrl = "https://www.googleapis.com/youtube/v3/&key= AIzaSyBEbEsm1R3asrUjS8pzXddROB-txKkF4tM";
+
+AIzaSyBEbEsm1R3asrUjS8pzXddROB-txKkF4tM
+
+// var youtubeSearchApiUrl = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails";
+
+// AIzaSyBEbEsm1R3asrUjS8pzXddROB-txKkF4tM
