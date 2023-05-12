79d4e858cad6222b1986d77c7e382374f28d7281
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-05-11 20:54:59 -0700

    added APIKeys and tested to see if the data was console logging

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
-var init = function () {
-  console.log("Hello World!");
-};
+//  this is the key for Omdb API
+// var movieTitleFunction = function (event) {
+// var searchBarEl = document.querySelector("#searchBar");
+// var movieTitle = searchBarEl.Value;
+var omdbApi = "http://www.omdbapi.com/?t=chicken&apikey=bd8b9b41";
 
-init();
+fetch(omdbApi).then(function (response) {
+  response.json().then(function (data) {
+    console.log(data);
+
+    //test syphon data
+    var rottenScore = data.Ratings[1].value;
+    console.log(rottenScore);
+  });
+});
+// };
+
+// var btn = document.querySelector("btn"); // finds the button!
+// btn.eventlistener("click", movieTitleFunction()); //when you click "btn" envoke function
+
+// youtube key : AIzaSyBEbEsm1R3asrUjS8pzXddROB-txKkF4tM
+
+var youtubeAPI =
+  "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBEbEsm1R3asrUjS8pzXddROB-txKkF4tM&q=blow+Trailer&type=video";
+
+fetch(youtubeAPI).then(function (response) {
+  response.json().then(function (data) {
+    console.log(data.items[0].id.videoId);
+  });
+});
